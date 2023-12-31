531ac4785e5c6681caa277b9d975ac1c13558979
Samuca <samuelmadeiradev@gmail.com>
2023-12-31 16:53:30 -0300

    Criando crud para notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,50 @@
 class NotesController < ApplicationController
-end
+    before_action :set_note, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @notes = Note.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @note = Note.new
+    end
+  
+    def create
+      @note = Note.new(note_params)
+      if @note.save
+        redirect_to @note, notice: 'Nota criada com sucesso.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+    end
+  
+    def update
+      if @note.update(note_params)
+        redirect_to @note, notice: 'Nota atualizada com sucesso.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @note.destroy
+      redirect_to notes_url, notice: 'Nota excluída com sucesso.'
+    end
+  
+    private
+  
+    def set_note
+      @note = Note.find(params[:id])
+    end
+  
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
+  end
+  
